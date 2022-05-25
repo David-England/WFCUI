6bd22d43df242aae98e9d153d208e3e1d9db192d
David England <david.england.de@googlemail.com>
2022-05-25 10:00:07 +0100

    Initial commit

---
+++ WFCUI/DatafileManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace WFCUI
+{
+    internal class DatafileManager : IDisposable
+    {
+        public string Filepath { get; init; }
+        public string FilenameNoExtension { get; init; }
+        public string Extension { get; init; }
+        
+        internal DatafileManager(string filepath)
+        {
+            Filepath = filepath;
+            FilenameNoExtension = Path.GetFileNameWithoutExtension(filepath);
+            Extension = Path.GetExtension(filepath);
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(filepath)); // possibly not thread safe
+
+            PopulateSamplesDirectory();
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                Directory.Delete("samples", recursive: true);
+            }
+            catch (IOException)
+            {
+                return;
+            }
+        }
+
+        private void PopulateSamplesDirectory()
+        {
+            Directory.CreateDirectory("samples");
+            File.Copy($"{FilenameNoExtension}{Extension}", $"samples\\{FilenameNoExtension}{Extension}", overwrite: true);
+        }
+    }
+}
---
+++ WFCUI/MagicZone.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WaveFunctionCollapse;
+
+namespace WFCUI
+{
+    /// <summary>
+    /// Interaction logic for MagicZone.xaml
+    /// </summary>
+    public partial class MagicZone : UserControl
+    {
+        public MagicZone()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("gdi32.dll")]
+        private extern static bool DeleteObject(IntPtr pointer);
+
+        protected override void OnDrop(DragEventArgs e)
+        {
+            base.OnDrop(e);
+
+            try
+            {
+                using (DatafileManager dm = new DatafileManager(GrabDataLocation(e)))
+                {
+                    BitmapSource bi = RunAlgorithm(dm);
+                    (new Display(dm.FilenameNoExtension, bi)).Show();
+                }
+                
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "You've got a problem....", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            static string GrabDataLocation(DragEventArgs drag)
+            {
+                if (!drag.Data.GetDataPresent(DataFormats.FileDrop)) throw new Exception("No data present");
+
+                drag.Effects = DragDropEffects.Copy;
+                drag.Handled = true;
+
+                return ((string[])drag.Data.GetData(DataFormats.FileDrop))[0];
+            }
+
+            static BitmapSource RunAlgorithm(DatafileManager dfManager)
+            {
+                if (dfManager.Extension != ".png") throw new Exception($"Dropped file is a \"{dfManager.Extension}\"; we only accept \".png\"s here.");
+
+                Bitmap bmap = Top.Fire(dfManager.FilenameNoExtension);
+                return (BitmapSource)ConvertOldBitmapToNew(bmap);
+            }
+        }
+
+        private static BitmapSource ConvertOldBitmapToNew(Bitmap bmap)
+        {
+            IntPtr pointerToBitmap = bmap.GetHbitmap();
+
+            try
+            {
+                // ASSUME this method never needs palette, sourceRect or sizeOptions specified.
+                return Imaging.CreateBitmapSourceFromHBitmap(pointerToBitmap, palette: IntPtr.Zero,
+                    sourceRect: Int32Rect.Empty, sizeOptions: BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                DeleteObject(pointerToBitmap);
+            }
+        }
+    }
+}
